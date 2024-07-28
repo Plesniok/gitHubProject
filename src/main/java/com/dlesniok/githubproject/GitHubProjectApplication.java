a5a95a208270de49a62cf480c5cf07790d4d447b
Damian Lesniok <dlesniok@sgu.com.pl>
2024-07-28 17:50:04 +0200

    first commit

---
+++ src/main/java/com/dlesniok/githubproject/GitHubProjectApplication.java
@@ -0,0 +1,14 @@
+package com.dlesniok.githubproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubProjectApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubProjectApplication.class, args);
+    }
+
+}
